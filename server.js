e7615ee8c93b57a960181ed0e695a4b736318c20
brhom00 <121161879+brhom00@users.noreply.github.com>
2026-08-18 11:12:33 +0300

    Initialize Express server with static file serving

---
+++ server.js
@@ -0,0 +1,23 @@
+
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// تشغيل ملفات الموقع
+app.use(express.static(__dirname));
+
+// الصفحة الرئيسية
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
